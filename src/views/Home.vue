c92a842e074d8775b9d138df2212ca621a6c6365
Dominic Foti <dominic@fotijr.com>
2019-01-05 19:36:43 -0500

    Added favicon and social media preview info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/walker.png">
     <HelloWorld msg="Step Converter"/>
   </div>
 </template>
